adcad6ce26084481da3cc0e2b1b110f009a4da0c
Suchitra Sahoo <123376847+Suchitra-Sahoo@users.noreply.github.com>
2024-05-14 08:17:25 +0530

    Revert "Image gallery"

--- script.js
+++ script.js
@@ -3,50 +3,6 @@ let navbar = document.querySelectorAll('.navbar');
 let Contact = document.querySelectorAll('#contact');
 let navContact = document.querySelectorAll('#nav--contact');
 
-// Scription for image gallery 
-let items = document.querySelectorAll('.slider .item');
-let active = 3;
-function loadShow(){
-    items[active].style.transform = `none`;
-    items[active].style.zIndex = 1;
-    items[active].style.filter = 'none';
-    items[active].style.opacity = 1;
-    // show after
-    let stt = 0;
-    for(var i = active + 1; i < items.length; i ++){
-        stt++;
-        items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-     stt = 0;
-    for(var i = (active - 1); i >= 0; i --){
-        stt++;
-        items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-}
-loadShow();
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-next.onclick = function(){
-   active = active + 1 < items.length ?  active + 1 : active;
-   loadShow();
-}
-prev.onclick = function(){
-    active = active - 1 >= 0 ? active -1 : active;
-    loadShow();
-}
-
-
-
-
-=======
-
-
 menuIcon.forEach(icon => {
   icon.onclick = () => {
     icon.classList.toggle('bx-x');
@@ -67,7 +23,6 @@ document.querySelectorAll('.navbar a').forEach(link => {
     });
   });
 });
-
 
 
 // Add an "active" class to the clicked link
@@ -195,7 +150,6 @@ form.addEventListener('submit', (event) => {
   form.reset();
   alert('Your message has been sent. Thank you!');
 });
-
  
 
 
@@ -284,4 +238,3 @@ executeRating(ratingStars);
 
 
   })();
-
