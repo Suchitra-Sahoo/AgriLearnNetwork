c7120064eb3646369b28d3719055c875bafbd891
Shivanipandey31 <sp6002936@gmail.com>
2024-05-20 00:34:13 +0530

    added calc.

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ ScrollReveal({
     delay:200
 });
 
-ScrollReveal().reveal('.home-content, .heading',{ origin:'top' });
+ScrollReveal().reveal('.home-content, .heading ,.calculator .heading',{ origin:'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{ origin:'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img',{ origin:'left' });
 ScrollReveal().reveal('.home-content p, .about-content',{ origin:'left' });
